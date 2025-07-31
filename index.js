58efe46929c0984b9250efcb4efe2e7a04204971
mirali vadhiya <miralivadhiya25@gmail.com>
2025-07-31 15:10:22 +0530

    [mv][data type]

---
+++ index.js
@@ -0,0 +1,46 @@
+//nn bb ss u
+let a=10;
+let b=null;
+let c=true;
+let d=BigInt("567");
+let e="javasript";
+let i="javasript";
+let f=Symbol("js");
+let h=Symbol("js");
+let g;
+console.log(a,b,c,d,e,f,g);
+
+console.log(e==i);
+console.log(f==h);
+console.log(typeof h);
+
+console.log(BigInt("567") + BigInt("3"));
+
+
+
+
+console.log("mirali")
+
+//non primitive type
+const arr=[1,2,3,4];
+const arr2=["hello","hii"];
+console.log(arr[2]);
+const user={
+    username:"mirali",
+    age:25,
+    isstudent:true,
+    hobbies:["reading","dancing"],
+    "email add":"text$text.com",
+};
+console.log(user.age);
+console.log(user["isstudent"]);
+console.log(user["email add"]);
+
+function sum(){
+    return 5+5;
+}
+const result=sum();
+console.log(result);
+
+
+
