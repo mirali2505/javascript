91fe70da3ee3d0d8d7af637bd6c1defa93238664
mirali vadhiya <miralivadhiya25@gmail.com>
2025-08-25 12:19:30 +0530

    [mv][task complete]

--- index.js
+++ index.js
@@ -1,46 +1,94 @@
 //nn bb ss u
-let a = 10;
-let b = null;
-let c = true;
-let d = BigInt("567");
-let e = "javasript";
-let i = "javasript";
-let f = Symbol("js");
-let h = Symbol("js");
-let g;
-console.log(a, b, c, d, e, f, g);
+// let a = 10;
+// let b = null;
+// let c = true;
+// let d = BigInt("567");
+// let e = "javasript";
+// let i = "javasript";
+// let f = Symbol("js");
+// let h = Symbol("js");
+// let g;
+// console.log(a, b, c, d, e, f, g);
 
-console.log(e == i);
-console.log(f == h);
-console.log(typeof h);
+// console.log(e == i);
+// console.log(f == h);
+// console.log(typeof h);
 
-console.log(BigInt("567") + BigInt("3"));
-
-
+// console.log(BigInt("567") + BigInt("3"));
 
 
-console.log("mirali")
 
-//non primitive type
-const arr = [1, 2, 3, 4];
-const arr2 = ["hello", "hii"];
-console.log(arr[2]);
-const user = {
-    username: "mirali",
-    age: 25,
-    isstudent: true,
-    hobbies: ["reading", "dancing"],
-    "email add": "text$text.com",
-};
-console.log(user.age);
-console.log(user["isstudent"]);
-console.log(user["email add"]);
 
-function sum() {
-    return 5 + 5;
-}
-const result = sum();
+// console.log("mirali")
+
+// //non primitive type
+// const arr = [1, 2, 3, 4];
+// const arr2 = ["hello", "hii"];
+// console.log(arr[2]);
+// const user = {
+//     username: "mirali",
+//     age: 25,
+//     isstudent: true,
+//     hobbies: ["reading", "dancing"],
+//     "email add": "text$text.com",
+// };
+// console.log(user.age);
+// console.log(user["isstudent"]);
+// console.log(user["email add"]);
+
+// function sum() {
+//     return 5 + 5;
+// }
+// const result = sum();
+// console.log(result);
+
+
+
+/*task 2*/
+const students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [75, 80, 85] },
+  { name: "Charlie", scores: [90, 95, 85] },
+  { name: "Jack", scores: [100, 100, 100] },
+];
+let output = students.map((a) => {
+  let total = a.scores.reduce((sum, scores) => sum + scores, 0);
+  return { name: a.name, total: total };
+});
+
+let result = output.filter((a) => a.total > 250);
+
 console.log(result);
 
 
-
+/*task 3*/
+const input = [
+  {
+    name: "John",
+    age: 13,
+  },
+  {
+    name: "Mark",
+    age: 56,
+  },
+  {
+    name: "Rachel",
+    age: 45,
+  },
+  {
+    name: "Nate",
+    age: 67,
+  },
+  {
+    name: "Jennifer",
+    age: 65,
+  },
+];
+let Age = input.map((b) => b.age);
+console.log(Age);
+let max = Math.max(...Age);
+console.log(max);
+let min = Math.min(...Age);
+console.log(min);
+let diff = max - min;
+console.log(diff);
--- prectice.js
+++ prectice.js
@@ -12,37 +12,62 @@
 //     }
 // }
 
-const school = {
-  name: "Sunrise High School",
-  address: {
-    city: "Junagadh",
-    state: "Gujarat",
-  },
-  classes: [
-    {
-      grade: 10,
-      students: [
-        { name: "Ayaan", marks: { math: 82, science: 76 } },
-        { name: "Sara", marks: { math: 95, science: 89 } }
-      ]
-    },
-    {
-      grade: 11,
-      students: [
-        { name: "Rahul", marks: { math: 78, science: 85 } },
-        { name: "Meena", marks: { math: 88, science: 92 } }
-      ]
-    }
-  ]
-};
+// const school = {
+//   name: "Sunrise High School",
+//   address: {
+//     city: "Junagadh",
+//     state: "Gujarat",
+//   },
+//   classes: [
+//     {
+//       grade: 10,
+//       students: [
+//         { name: "Ayaan", marks: { math: 82, science: 76 } },
+//         { name: "Sara", marks: { math: 95, science: 89 } }
+//       ]
+//     },
+//     {
+//       grade: 11,
+//       students: [
+//         { name: "Rahul", marks: { math: 78, science: 85 } },
+//         { name: "Meena", marks: { math: 88, science: 92 } }
+//       ]
+//     }
+//   ]
+// };
 
-for(const clas of school.classes){
-  for(const stud of clas.students){
-    const marks=Object.entries(stud.marks);
-    console.log("grade"+clas.grade+"-"+stud.name+":"+marks);
-    
+// for(const clas of school.classes){
+//   for(const stud of clas.students){
+//     const marks=Object.entries(stud.marks);
+//     console.log("grade"+clas.grade+"-"+stud.name+":"+marks);
+
+//   }
+// }
+
+// const users = [
+//   { name: 'Alice', age: 30, active: true },
+//   { name: 'Bob', age: 25, active: false },
+//   { name: 'Charlie', age: 30, active: true },
+//   { name: 'David', age: 25, active: true }
+// ];
+
+let subJectMark = { java: 90, python: 92, c: 89, react: 99 };
+function result(mark) {
+  let sum = 0;
+  const arrOfMark = Object.value(mark);
+  console.log(arrOfMark);
+  for (let i = 0; i < arrOfMark.length; i++) {
+    sum += arrOfMark[i];
   }
+  console.log({ sum });
+  const precentage = (sum * 100) / (arrOfMark.length * 100);
+  return precentage;
+
+
 }
+
+
+
 
 
 
